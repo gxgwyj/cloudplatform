d588b2951cf5ab24d2848bbe74b063f6bd6bbf85
gaoxugang1 <1253652573@qq.com>
2017-02-13 21:44:31 +0800

    desc add

--- src/main/java/com/xinguoren/coolpen/cloud/web/controller/SearchController.java
+++ src/main/java/com/xinguoren/coolpen/cloud/web/controller/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
+ * 搜索引擎
  * Created by Administrator on 2016/12/12.
  */
 @Controller
