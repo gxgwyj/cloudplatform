c94690d37e1d272c43c9112b2957303c885a4701
gxgwyj <498868779@qq.com>
2017-10-12 18:50:59 +0800

    添加公共基类

---
+++ src/main/java/com/xinguoren/coolpen/cloud/web/base/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.xinguoren.coolpen.cloud.web.base.controller;
+
+/**
+ * Created by Administrator on 2017/10/12.
+ */
+public class BaseController {
+}
---
+++ src/main/java/com/xinguoren/coolpen/cloud/web/base/service/BaseService.java
@@ -0,0 +1,7 @@
+package com.xinguoren.coolpen.cloud.web.base.service;
+
+/**
+ * Created by Administrator on 2017/10/12.
+ */
+public class BaseService {
+}
